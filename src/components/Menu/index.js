bb78d2d3e1d19e429e970a45a5463235ae1ca297
Dany <dany.jc@yahoo.com>
2020-05-12 15:47:19 +0200

    :sparkles: add new menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,141 +1,47 @@
 import React from 'react';
-import clsx from 'clsx';
-import { useTheme } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
-import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
 import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
-import ContactsIcon from '@material-ui/icons/Contacts';
-import SettingsIcon from '@material-ui/icons/Settings';
-import ReceiptIcon from '@material-ui/icons/Receipt';
-import CreateIcon from '@material-ui/icons/Create';
-import TextField from '@material-ui/core/TextField';
 import { style } from './style';
 
 const Menu = ({ main }) => {
     const classes = style();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
 
     return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar
-                position="fixed"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
             >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        className={clsx(classes.menuButton, {
-                            [classes.hide]: open,
-                        })}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" noWrap>
-                        Dofus - Avis de recherche
-                    </Typography>
-                </Toolbar>
-                <Toolbar>
-                    <form className={classes.root} noValidate autoComplete="off">
-                        <TextField id="outlined-basic" label="Rechercher" variant="outlined" />
-                    </form>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                variant="permanent"
-                className={clsx(classes.drawer, {
-                    [classes.drawerOpen]: open,
-                    [classes.drawerClose]: !open,
-                })}
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" noWrap>
+              Dofus - Avis de recherche
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Rechercher"
                 classes={{
-                    paper: clsx({
-                        [classes.drawerOpen]: open,
-                        [classes.drawerClose]: !open,
-                    }),
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
                 }}
-            >
-                <div className={classes.toolbar}>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-                    </IconButton>
-                </div>
-                <Divider />
-                <List>
-                    <Link className={classes.link} to='/account'>
-                        <ListItem button key='comptes'>
-                            <ListItemIcon><AccountBalanceIcon /></ListItemIcon>
-                            <ListItemText primary='Comptes' />
-                        </ListItem>
-                    </Link>
-                    <Link className={classes.link} to='/transactions'>
-                        <ListItem button key='transactions'>
-                            <ListItemIcon><ReceiptIcon /></ListItemIcon>
-                            <ListItemText primary='Transactions' />
-                        </ListItem>
-                    </Link>
-                    <Link className={classes.link} to='/contacts'>
-                        <ListItem button key='contacts'>
-                            <ListItemIcon><ContactsIcon /></ListItemIcon>
-                            <ListItemText primary='Contacts' />
-                        </ListItem>
-                    </Link>
-                    <Link className={classes.link} to='/contracts'>
-                        <ListItem button key='contracts'>
-                            <ListItemIcon><CreateIcon /></ListItemIcon>
-                            <ListItemText primary='Smart Contrats' />
-                        </ListItem>
-                    </Link>
-                </List>
-                <Divider />
-                <List>
-                    <Link className={classes.link} to='/settings'>
-                        <ListItem button key='settings'>
-                            <ListItemIcon><SettingsIcon /></ListItemIcon>
-                            <ListItemText primary='Paramètres' />
-                        </ListItem>
-                    </Link>
-                    <Link className={classes.link} to='/logout'>
-                        <ListItem button key='logout'>
-                            <ListItemIcon><ExitToAppIcon /></ListItemIcon>
-                            <ListItemText primary='Déconnexion' />
-                        </ListItem>
-                    </Link>
-                </List>
-            </Drawer>
-            <main className={classes.content}>
-                <div className={classes.toolbar}>
-                    {main}
-                </div>
-            </main>
-        </div>
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
 }
 
--- src/components/Menu/style.js
+++ src/components/Menu/style.js
@@ -5,72 +5,52 @@ const drawerWidth = 240;
 
 export const style = makeStyles(theme => ({
     root: {
-        display: 'flex',
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent:'space-between',
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
+        flexGrow: 1,
     },
     menuButton: {
-        marginRight: 36,
+        marginRight: theme.spacing(2),
     },
-    hide: {
+    title: {
+        flexGrow: 1,
         display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-    },
-    drawerOpen: {
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    drawerClose: {
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        overflowX: 'hidden',
-        width: theme.spacing(7) + 1,
         [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9) + 1,
+            display: 'block',
         },
     },
-    toolbar: {
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+       
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: theme.spacing(0, 1),
-        ...theme.mixins.toolbar,
+        justifyContent: 'center',
     },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
+    inputRoot: {
+        color: 'inherit',
     },
-    link: {
-        color: APP_COLORS.dark,
-        textDecoration: 'none'
-    },
-    userIcon: {
-        fontSize: '45px',
-        marginLeft: '8px',
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
     },
 }));
