5b5719256c5569eca8eeed0b69e29b4c495b0b55
unknown <ddw_github@de-weerd.name>
2020-05-25 12:59:13 +0200

    changed loading animation

--- src/pages/AreaMonsters/index.js
+++ src/pages/AreaMonsters/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ApiCall from '../../components/ApiCall'
 import Monsters from '../Monsters'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { style } from './style'
 
 const AreaMonsters = ({ req }) => {
+    const classes = style()
+    
     const tab = ApiCall(req).Monsters
 
     if (tab !== undefined) {
@@ -11,7 +15,9 @@ const AreaMonsters = ({ req }) => {
         )
     } else {
         return (
-            "Loading"
+            <div className={classes.loading}>
+                <CircularProgress color="secondary" />
+            </div>
         )
     }
 
---
+++ src/pages/AreaMonsters/style.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const style = makeStyles(theme => ({
+    loading: {
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
+}))
